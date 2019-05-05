3efee37ee48028ede482e4b77cd72a8b5aef61ad
rykyou <rkim1214@gmail.com>
2019-05-04 20:35:19 -0400

    Finished vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,26 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  //match takes in regular expression that checks to see if what's passed in as arg matches str
+  //returns arr of all matches found, if str has any char included between []
+  //if no match found, will return null
+  //2 options added to regex: g-> makes sure we don't stop at first match we find inside str; i-> "case insensitive"
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+
+// function vowels(str) {
+//   //iterative solution
+//   //counter value with default value of 0, increment if come across vowel
+//   let count = 0;
+
+//   for (let char of str.toLowerCase()) {
+//     ['a', 'e', 'i', 'o', 'u'].includes(char) ? count++ : 0;
+//   }
+
+//   return count;
+// }
