e656f85d2609162bb87005b5a0b3a6c805767374
rykyou <rkim1214@gmail.com>
2019-05-11 17:31:15 -0400

    Finished qfroms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,53 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.first = new Stack();
+    this.second = new Stack();
+  }
+
+  add(record) {
+    this.first.push(record);
+  }
+
+  remove() {
+    //iterate through all records in first stack and while peek method returns truthy value, pop record out of 1st stack and push into 2nd stack
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+
+    //save ref to this record to return later after restoring state of stacks
+    const record = this.second.pop();
+
+    //going back to original state
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+
+    return record;
+  }
+
+  peek() {
+    //this == {first: Stack/Array, second: Stack}
+
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+
+    const record = this.second.peek();
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+
+    return record;
+  }
+}
+
+const q = new Queue();
+q.add('a');
+q.add('b');
+q.peek();
 
 module.exports = Queue;
