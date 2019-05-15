196e3d900cdf6d93c4727aec4fad9bb41ccf3359
rykyou <rkim1214@gmail.com>
2019-05-15 12:22:27 -0400

    Finished linkedlist

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -19,7 +19,10 @@ class LinkedList {
     //don't want to overwrite existing node if there is one, so pass this.head (existing node) as 2nd arg to new node being created. New node will be new head of linked list and it'll hold ref to other node that existed before
     // const node = new Node(data, this.head);
     // this.head = node;
-    this.head = new Node(data, this.head);
+
+    // this.head = new Node(data, this.head);
+
+    this.insertAt(data, 0);
   }
 
   size() {
@@ -36,22 +39,25 @@ class LinkedList {
   }
 
   getFirst() {
+    // return this.getAt(0);
     return this.head;
   }
 
   getLast() {
-    if (!this.head) { 
-      return null;
-    }
+    // if (!this.head) { 
+    //   return null;
+    // }
 
-    let node = this.head;
+    // let node = this.head;
 
-    while (node) {
-      if (!node.next) {
-        return node;
-      }
-      node = node.next;
-    }
+    // while (node) {
+    //   if (!node.next) {
+    //     return node;
+    //   }
+    //   node = node.next;
+    // }
+
+    return this.getAt(this.size() - 1);
   }
 
   clear() {
@@ -107,6 +113,62 @@ class LinkedList {
       node = node.next;
     }
     return null;
+  }
+
+  removeAt(index) {
+    if (!this.head) {
+      return;
+    }
+
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+
+    const previous = this.getAt(index - 1);
+    if (!previous || !previous.next) {
+      return;
+    }
+    previous.next = previous.next.next;
+  }
+
+  insertAt(data, index) {
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    //if this.getAt... returns falsey value, previous = this.getLast()
+    const previous = this.getAt(index - 1) || this.getLast();
+    const node = new Node(data, previous.next);
+    previous.next = node;
+  }
+
+  forEach(fn) {
+    let node = this.head;
+    let counter = 0;
+    while (node) {
+      //fn(node, index)
+      fn(node, counter);
+      node = node.next;
+      counter++;
+    }
+  }
+
+  //iterator function
+  //defines generator fn with key of Symbol.iterator
+  *[Symbol.iterator]() {
+    let node = this.head;
+    while (node) {
+      //for every step along linked list, yield node
+      yield node;
+      node = node.next;
+    }
   }
 }
 
